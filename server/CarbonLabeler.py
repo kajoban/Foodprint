662c023420a57c4c1857b3d47e8286e8cb8cae3b
kajoban <kuhapark@mcmaster.ca>
2020-08-15 17:20:09 -0400

    updated server files

--- server/CarbonLabeler.py
+++ server/CarbonLabeler.py
@@ -5,6 +5,8 @@ import flask
 from FoodLabel import FoodLabel
 
 service_json = 'Carbon_Foodprint.json'
+
+
 class SmartLabels(object):
 
     def __init__(self, labels):
@@ -14,24 +16,24 @@ class SmartLabels(object):
         self.smartify()
         self.smartLabels = [label.strip().lower() for label in labels]
         self.smartLabels = list(set(self.smartLabels))
-        
 
     def smartify(self):
         pass
         # add some NLP for categorization
-    
+
     def __iter__(self):
-        return self
-    
-    def __next__(self):
-        self.index += 1
-        if self.index >= len(self.smartLabels):
-            self.index = -1
-            raise StopIteration
-        else:
-            return self.smartLabels[self.index]
-    def __str__(self):
-        return str(self.smartLabels)
+        return iter(self.smartLabels)
+
+   # def __next__(self):
+    #    self.index += 1
+     #   if self.index >= len(self.smartLabels):
+      #      self.index = -1
+      #      raise StopIteration
+      #  else:
+       #     return self.smartLabels[self.index]
+   # def __str__(self):
+    #    return str(self.smartLabels)
+
 
 creds = credentials.Certificate(service_json)
 firebase_admin.initialize_app(creds)
@@ -41,16 +43,16 @@ db = firestore.client()
 # Find the amount of CO2 emissions in a food based off image labels
 #
 #
+
+
 class CarbonLabeler():
-    
-    
+
     # initialize labels and connection to firebase
     def __init__(self, labels):
 
-        self.footprints=[]
+        self.footprints = []
         self.smartLabels = SmartLabels(labels)
         print(self.smartLabels)
-
 
     def getFootprints(self):
 
@@ -61,15 +63,8 @@ class CarbonLabeler():
             item_ref = recognized_footprints.document(label)
             item = item_ref.get()
             if item.exists:
-                print(f'Document data: {item.to_dict()}')
                 self.footprints.append(item.to_dict())
             else:
                 print(u'Not recognized, sorry!')
 
         return self.footprints
-
-
-
-
-
-
