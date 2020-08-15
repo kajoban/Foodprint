ee688d5bb2a777b81a63c1e42a2aa88ed99a06e9
kajoban <kuhapark@mcmaster.ca>
2020-08-14 22:11:13 -0400

    added simple swiping

--- client/App.js
+++ client/App.js
@@ -1,21 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Container, Content } from "native-base";
+import Swiper from "react-native-swiper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is a cool project!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Content>
+        <Swiper loop={false} index={0} showsPagination={false}>
+          <View style={styles.slideDefault}>
+            <Text style={styles.text}>Camera</Text>
+          </View>
+          <View style={styles.slideDefault}>
+            <Text style={styles.text}>Table</Text>
+          </View>
+        </Swiper>
+      </Content>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  slideDefault: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
   },
 });
